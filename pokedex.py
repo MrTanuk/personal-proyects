df6982162fcfe6ac6fc41c4b083e2a32ec8fb342
Luis Ramírez <louismario.rr@gmail.com>
2024-06-23 19:56:30 -0400

    Improve code and change prettytable by pandas

--- pokedex.py
+++ pokedex.py
@@ -1,42 +1,37 @@
 import csv
 from prettytable import PrettyTable 
+import pandas as pd
 
-pokedex = PrettyTable()
+pokedex = pd.read_csv("./pokemon_datas/pokemon.csv")
 
-with open(r"pokemon_datas/pokemon.csv", newline="") as rd:
-    file = csv.reader(rd)
-    pokedex.field_names = ["#", "Nombre", "Tipo 1", "Tipo 2", "Total", "HP",\
-                       "Ataque", "Defensa","Ataque Especial", "Defensa Especial",\
-                        "Velocidad", "Generación", "Legendario"]
-    for fila in file:
-        pokedex.add_row(fila)
+def main():
+    while True:
+        print("1. Mostras Pokedex completa")
+        print("2. Busqueda por ID")
+        print("3. Busqueda por nombre:")
+        print("4. Filtrar números de IDs de inicio a fin")
+        print("5. Finalizar")
 
-while True:
-    print("1. Busqueda por ID")
-    #print("2. Busqueda por nombre:")
-    print("3. Filtrar números de IDs de inicio a fin")
-    print("4. Mostras Pokedex completa")
-    print("5. Finalizar")
+        option = int(input("seleccione: "))
+        match option:
+            case 1:
+                pd.set_option('display.max_rows', None)
+                print(pokedex,"\n")
+            case 2:
+                id = int(input("Escriba la id: "))
+                pokemon_by_id = pokedex.loc[pokedex['#'] == id]
+                print(pokemon_by_id,"\n")
+            case 3:
+                name = input("Escriba el nombre: ")
+                pokemon_by_name = pokedex.loc[pokedex['Name'] == name]
+                print(pokemon_by_name,"\n")
+            case 4:
+                id_01 = int(input("Escriba el inicio: "))
+                id_02 = int(input("Escriba el fin: "))
+                pokemons_by_id =  pokedex.loc[pokedex['#'].between(id_01, id_02)]
+                print(pokemons_by_id,"\n")
+            case 5:
+                break
 
-    option = int(input())
-
-    match option:
-        case 1: 
-            id = int(input("ID: "))
-            print(pokedex.get_string(start=id-1, end=id))
-        
-        #case 2:
-            #nombre_buscar = input("Nombre del Pokemón: ")
-            #for name in pokedex:
-                #if name[0] == 2:
-
-        case 3:
-            id_1 = int(input("ID de inicio: "))
-            id_2 = int(input("ID de fin: "))
-            print(pokedex.get_string(start=id_1-1, end=id_2))
-        
-        case 4:
-            print(pokedex)
-
-        case 5:
-            break
+if __name__ == "__main__":
+    main()
